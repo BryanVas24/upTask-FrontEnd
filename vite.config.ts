fb986979915fcc9c41fd7f4b2dac7977c1811687
BryanVas24 <bryanvasquez2406@gmail.com>
2024-06-16 17:30:43 -0600

    agregando que vite reconozca la @ en las rutas

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+//esto se importa con el comando -> npm i -D @types/node
+import { fileURLToPath, URL } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //esto sirve para indicar hacia que ruta ir cuando pones la @ (puede ser cualquier cosa, no solo una @)
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
