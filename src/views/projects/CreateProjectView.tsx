6151a329de3863d138c4e647c34ec04fa6f225f8
BryanVas24 <bryanvasquez2406@gmail.com>
2024-06-17 16:03:34 -0600

    Creando el formulario de crear proyectos (sin terminar)

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,18 +1,50 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 const CreateProjectView = () => {
+  //estos son los valores iniciales del form
+  const initialValues = {
+    projectName: "",
+    clientName: "",
+    description: "",
+  };
+  //esto es de react-hook form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: initialValues });
+  //esta es la función que se pasa en el handle submit de react-hook-form
+  const handleForm = (data) => {
+    console.log(data);
+  };
+
+  //-----el no validate en el form deshanilita la validación de hmtl5 para que la hagas vos
   return (
     <>
-      <h1 className="text-5xl font-black">Crear proyecto</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">
-        LLena el siguiente formulario para crear un proyecto
-      </p>
-      <div className="my-5">
-        <Link
-          to={"/"}
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold transition-colors cursor-pointer"
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-5xl font-black">Crear proyecto</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">
+          LLena el siguiente formulario para crear un proyecto
+        </p>
+        <div className="my-5">
+          <Link
+            to={"/"}
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold transition-colors cursor-pointer"
+          >
+            Volver a inicio
+          </Link>
+        </div>
+        <form
+          className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+          onSubmit={handleSubmit(handleForm)}
+          noValidate
         >
-          Nuevo proyecto
-        </Link>
+          <input
+            type="submit"
+            value="Crear proyecto"
+            className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors"
+          />
+        </form>
       </div>
     </>
   );
