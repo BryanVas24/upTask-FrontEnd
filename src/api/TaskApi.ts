19c0102a66fff6c95f5ec7abc80ab45e156a3aa9
BryanVas24 <bryanvasquez2406@gmail.com>
2024-06-27 13:48:36 -0600

    Obteniendo datos de la tarea a editar

--- src/api/TaskApi.ts
+++ src/api/TaskApi.ts
@@ -1,15 +1,34 @@
 import api from "@/lib/axios";
 import { isAxiosError } from "axios";
-import { Project, TaskFormData } from "../types";
+import { Project, Task, TaskFormData } from "../types";
 
 type TaskApi = {
   formData: TaskFormData;
   projectId: Project["_id"];
+  taskId: Task["_id"];
 };
-export async function createTask({ formData, projectId }: TaskApi) {
+export async function createTask({
+  formData,
+  projectId,
+}: Pick<TaskApi, "formData" | "projectId">) {
   try {
     const url = `/projects/${projectId}/tasks`;
     const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function getTaskByID({
+  projectId,
+  taskId,
+}: Pick<TaskApi, "projectId" | "taskId">) {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}`;
+    const { data } = await api(url);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
--- src/components/tasks/EditTaskData.tsx
+++ src/components/tasks/EditTaskData.tsx
@@ -1,10 +1,21 @@
-import { useLocation } from "react-router-dom";
+import { getTaskByID } from "@/api/TaskApi";
+import { useQuery } from "@tanstack/react-query";
+import { useLocation, useParams } from "react-router-dom";
 
 export default function EditTaskData() {
+  //extrayendo el id del proyecto
+  const params = useParams();
+  const projectId = params.projectId!;
   //tomadno el id de la url
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const editTask = queryParams.get("editTask");
-  console.log(editTask);
+  const taskId = queryParams.get("editTask")!;
+
+  const { data } = useQuery({
+    queryKey: ["task", taskId],
+    queryFn: () => getTaskByID({ projectId, taskId }),
+  });
+
+  console.log(data);
   return <div></div>;
 }
