0ffe793b7c43a0d5bc308446f789614ee0d7e8f1
BryanVas24 <bryanvasquez2406@gmail.com>
2024-06-26 22:18:07 -0600

    Mostrando el estado de las tareas

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -8,6 +8,14 @@ type TaskListProps = {
 type GroupedTasks = {
   [key: string]: Task[];
 };
+//diccionario para traducir XD
+const StatusTranslations: { [key: string]: string } = {
+  pending: "Pendiente",
+  onHold: "En espera",
+  inProgress: "En progreso",
+  underReview: "En revisión",
+  conmpleted: "Completada",
+};
 //para que los arrays no esten vacios en la primera iteración
 const initialStatusGroups: GroupedTasks = {
   pending: [],
@@ -29,6 +37,11 @@ const TaskList = ({ tasks }: TaskListProps) => {
       <div className="flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32">
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
+            <h3
+              className={`capitalize text-xl text-center border-slate-300 bg-white p-3 border-t-8`}
+            >
+              {StatusTranslations[status]}
+            </h3>
             <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">
