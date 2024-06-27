50d404def8b76d5ddd82bf4b086d283b53cb19e5
BryanVas24 <bryanvasquez2406@gmail.com>
2024-06-26 22:31:29 -0600

    agregando colores a las tareas y pequeño fixing en statusTranslations,BorderStatusColors y initialStatusGroups

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -14,7 +14,15 @@ const StatusTranslations: { [key: string]: string } = {
   onHold: "En espera",
   inProgress: "En progreso",
   underReview: "En revisión",
-  conmpleted: "Completada",
+  completed: "Completada",
+};
+//esto es para cambiar color de bordes dinamicamente
+const BorderStatusColor: { [key: string]: string } = {
+  pending: "border-t-slate-500",
+  onHold: "border-t-red-500",
+  inProgress: "border-t-blue-500",
+  underReview: "border-t-amber-500",
+  completed: "border-t-emerald-500",
 };
 //para que los arrays no esten vacios en la primera iteración
 const initialStatusGroups: GroupedTasks = {
@@ -22,7 +30,7 @@ const initialStatusGroups: GroupedTasks = {
   onHold: [],
   inProgress: [],
   underReview: [],
-  conmpleted: [],
+  completed: [],
 };
 const TaskList = ({ tasks }: TaskListProps) => {
   const groupedTasks = tasks.reduce((acc, task) => {
@@ -38,7 +46,7 @@ const TaskList = ({ tasks }: TaskListProps) => {
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className="min-w-[300px] 2xl:min-w-0 2xl:w-1/5">
             <h3
-              className={`capitalize text-xl text-center border-slate-300 bg-white p-3 border-t-8`}
+              className={`capitalize text-xl text-center font-light border border-slate-300 bg-white p-3 border-t-8 ${BorderStatusColor[status]}`}
             >
               {StatusTranslations[status]}
             </h3>
