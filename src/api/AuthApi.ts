599428f55005bcbb4e67ccdd59b08411d526d154
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-07 13:24:14 -0600

    recibiendo el jwt

--- src/api/AuthApi.ts
+++ src/api/AuthApi.ts
@@ -5,6 +5,7 @@ import {
   ForgotPasswordForm,
   NewPasswordForm,
   RequestConfirmationCodeForm,
+  UserLoginForm,
   UserRegistrationForm,
 } from "../types";
 
@@ -12,6 +13,18 @@ export async function createAccount(formData: UserRegistrationForm) {
   try {
     const url = `/auth/create-account`;
     const { data } = await api.post<string>(url, formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+export async function authenticateUser(formData: UserLoginForm) {
+  try {
+    const url = `/auth/login`;
+    const { data } = await api.post<string>(url, formData);
+    localStorage.setItem("AUTH_TOKEN", data);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
