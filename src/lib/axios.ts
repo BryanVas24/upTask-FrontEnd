9dfb47cb82f8eb56274053295be9176598cd77a4
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-08 19:28:49 -0600

    evitando repetir enviar el token con interceptors de axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,5 +3,16 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
-
+/*los interceptors son propios de axios y basicamente son funciones
+ que se ejecutan antes o despues de una solicitud http*/
+//--REQUEST-- es antes de la solicitud y RESPONSE es para despues
+api.interceptors.request.use((config) => {
+  //tomando el jwt del locale storage
+  const JWtoken = localStorage.getItem("AUTH_TOKEN");
+  if (JWtoken) {
+    //envias los headers porque el backend pide un jwt como Bearer
+    config.headers.Authorization = `Bearer ${JWtoken}`;
+  }
+  return config;
+});
 export default api;
