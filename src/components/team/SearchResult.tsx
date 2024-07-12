3a822baf0122c7497b74b4214b67b38adf348b74
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-12 15:11:24 -0600

    Agregando el usuario al proyecto

--- src/components/team/SearchResult.tsx
+++ src/components/team/SearchResult.tsx
@@ -1,15 +1,37 @@
+import { AddUserToProject } from "@/api/TeamApi";
 import { TeamMember } from "@/types/index";
+import { useMutation } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 type SearchResultprops = {
   user: TeamMember;
 };
 function SearchResult({ user }: SearchResultprops) {
+  const params = useParams();
+  const projectId = params.projectId!;
+  const { mutate } = useMutation({
+    mutationFn: AddUserToProject,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      toast.success(data);
+    },
+  });
+  const handleAddUserToProject = () => {
+    const data = { projectId, id: user._id };
+    mutate(data);
+  };
   return (
     <>
       <p className="mt-10 text-center font-bold">Resultado:</p>
       <div className="flex flex-col gap-4 md:flex-row justify-evenly items-center">
         <p>{user.name}</p>
-        <button className="text-purple-600 hover:bg-purple-100 px-10 py-3 font-bold cursor-pointer">
+        <button
+          onClick={handleAddUserToProject}
+          className="text-purple-600 hover:bg-purple-100 px-10 py-3 font-bold cursor-pointer"
+        >
           Agregar al proyecto
         </button>
       </div>
