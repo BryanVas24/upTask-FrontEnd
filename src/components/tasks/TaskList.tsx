509e503ee99896d862b0733da3e7b7ba60ba38f3
BryanVas24 <bryanvasquez2406@gmail.com>
2024-07-19 21:45:55 -0600

    Creando componente para arrastrar y soltar (solo estilos)

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,6 +1,7 @@
 import { Task } from "@/types/index";
 import TaskCard from "./TaskCard";
 import { StatusTranslations } from "@/locales/es";
+import DropTask from "./DropTask";
 
 type TaskListProps = {
   tasks: Task[];
@@ -45,6 +46,7 @@ const TaskList = ({ tasks, canEdit }: TaskListProps) => {
             >
               {StatusTranslations[status]}
             </h3>
+            <DropTask />
             <ul className="mt-5 space-y-5">
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">
